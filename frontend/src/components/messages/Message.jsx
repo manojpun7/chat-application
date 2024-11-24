fc2cfc1317dafd1c698426974eaef308e3a4cb7f
Manoj <punmanoj2060@gmail.com>
2024-11-24 22:44:42 +0545

    search functionality hurrah

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -22,7 +22,7 @@ const Message = ({ message }) => {
           <img src={profilePic} alt="Tailwind css chat bubble component" />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+      <div className={`chat-bubble text-white ${bubbleBgColor} `}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
