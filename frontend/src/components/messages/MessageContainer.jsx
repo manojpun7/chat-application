26e301c7e5b791db5fdb2087aae4ce7bf12228d7
Manoj <punmanoj2060@gmail.com>
2024-11-24 20:20:06 +0545

    get conversations completed

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,36 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
-
+import useConversation from "../../zustand/useConversation";
 
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-     {
-      noChatSelected? (
-        <NoChatSelected/>
-      ):(
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
         <>
-        {/* header  */}
+          {/* header  */}
 
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">
-            To:{" "}
-            <span
-              className="text-gray-900
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">
+              To:{" "}
+              <span
+                className="text-gray-900
             font-bold"
-            >
-              john doe
+              >
+                {selectedConversation.fullName}
+              </span>
             </span>
-          </span>
-        </div>
-        <Messages />
-        <MessageInput />
-      </>
-      )
-     }
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
@@ -38,14 +41,13 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
-	return (
-		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 John  ❄</p>
-				<p>Select a chat to start messaging</p>
-				<TiMessages className='text-3xl md:text-6xl text-center' />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 John ❄</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
 };
-
