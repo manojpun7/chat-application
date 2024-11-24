3d77ad9bf81c1b651458831fc6aed9eb2e5630d2
Manoj <punmanoj2060@gmail.com>
2024-11-24 22:27:45 +0545

    get messages functionality completed hai ta haha

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import Message from './Message'
+import React, { useEffect, useRef } from "react";
+import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 10);
+  }, [messages]);
   return (
-    <div className='px-4 flex-1 overflow-auto'>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-    </div>
-  )
-}
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
 
-export default Messages
+      {loading && [...Array(5)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center ">send a message to start the conversation</p>
+      )}
+    </div>
+  );
+};
+
+export default Messages;
