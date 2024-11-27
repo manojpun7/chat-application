730ca5234a488d43be5b61d420cdd31a581e7437
Manoj <punmanoj2060@gmail.com>
2024-11-27 20:26:55 +0545

    deployment deployment !!!!!

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -9,8 +10,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve(); // this is for deployment purpose
 
 dotenv.config();
 
@@ -21,8 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // this is for deployment purpose
+app.get("*", (req, res) => {
+  // this is for deployment purpose
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); // this is for deployment purpose
+}); // this is for deployment purpose
+
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
 });
-  
