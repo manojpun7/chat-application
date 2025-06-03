5ac37a6631e1d429e6f0f07ef8d834230b5daf94
Manoj Pun <113597710+manojpun7@users.noreply.github.com>
2025-06-03 12:54:46 +0545

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -25,7 +25,7 @@ const useSignup = () => {
     if (!success) return;
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("https://chat-application-backend-gma3.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
