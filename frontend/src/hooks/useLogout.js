8c2836ff596edbc65fa58838218ae1a44c7d948f
Manoj Pun <113597710+manojpun7@users.noreply.github.com>
2025-06-03 12:54:18 +0545

    Update useLogout.js

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -8,7 +8,7 @@ const useLogout = () => {
   const logout = async () => {
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/logout", {
+      const res = await fetch("https://chat-application-backend-gma3.onrender.com/api/auth/logout", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
       });
