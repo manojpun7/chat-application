ca8051fe6961eceaafc806b2302511f5f8aee8ca
Manoj <punmanoj2060@gmail.com>
2025-06-03 14:27:17 +0545

    first method

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
-// import path from "path";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,7 +13,7 @@ import { app, server } from "./socket/socket.js";
 
 
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve(); // this is for deployment purpose
+const __dirname = path.resolve(); // this is for deployment purpose
 
 
 app.use(express.json());
@@ -23,12 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist"))); // this is for deployment purpose
-// app.get("*", (req, res) => {
-//   // this is for deployment purpose
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); // this is for deployment purpose
-// }); // this is for deployment purpose
-
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // this is for deployment purpose
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); // this is for deployment purpose
+}); 
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
